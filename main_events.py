41107c6b6df157805e514bcb733e808964b7bd52
qholness <qholness@gmail.com>
2015-11-07 18:13:22 -0600

    Update main_events.py
    Expanding the idea

--- main_events.py
+++ main_events.py
@@ -1,6 +1,18 @@
-import random
+from flask import Flask, render_template
+import main_events as me 
 
-def random_US_state():
-	return random.choice(["NJ", "IL", "CA", "MA"])
+app = Flask(__name__)
 
-	
+@app.route("/")
+def hello():
+	this_guy = me.player(state = me.random_US_state())
+	return """
+		<h2>Looks like you\'re getting placed in {0}!</h2>
+		<p>And you live in a {1} neighborhood</p>
+		""".format(this_guy.state, this_guy.neighborhood)
+
+    # Proposal: more granularity. Choose MSA within a STATE
+ #    state_choice = me.random_US_state()
+	# msa_choice = random_US_MSA(state_choice)
+if __name__ == "__main__":
+    app.run(debug=True)
